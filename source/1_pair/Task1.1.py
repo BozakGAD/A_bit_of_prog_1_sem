76187031ec73588f1fa49b77abeae57457c73a89
Bozak <129062311+BozakGAD@users.noreply.github.com>
2025-10-13 09:52:02 +0500

    Update_Task1.1.py
    Сделал менее глупое создание списков.

--- source/1_pair/Task1.1.py
+++ source/1_pair/Task1.1.py
@@ -1,15 +1,13 @@
-from random import choice,shuffle
+from random import choice
+from random import shuffle
 from string import ascii_uppercase
-st = []
-nu = []
-sy = []
-for i in ascii_uppercase:
-    st += i
-for i in range(0,10):
-    nu += str(i)
-for i in "!@#$%^&*()":
-    sy += str(i)
-ran_code = [choice(st)]+[choice(st)]+[choice(st)]+[choice(nu)]+[choice(nu)]+[choice(nu)]+[choice(sy)]+[choice(sy)]
+
+st = [i for i in ascii_uppercase]
+nu = [str(i) for i in range(1,10)]
+sy = [i for i in "!@#$%^&*()"]
+ran_code = ([choice(st)]+[choice(st)]+[choice(st)]
+            +[choice(nu)]+[choice(nu)]+[choice(nu)]
+            +[choice(sy)]+[choice(sy)])
 shuffle(ran_code)
 s = "".join(ran_code)
 print(s)
