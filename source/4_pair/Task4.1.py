0b071577924705784a8b694fb6bcb728443a3c90
Bozak <129062311+BozakGAD@users.noreply.github.com>
2025-10-23 20:07:52 +0500

    Docstrings_Task4.1.py
    Added docstrings for class and standardized the name of solution.

---
+++ source/4_pair/Task4.1.py
@@ -0,0 +1,34 @@
+class Book:
+    """
+    Класс, представляющий книгу.
+
+    Attributes:
+        title (str): Название книги.
+        author (str): Автор книги.
+        year (str): Год издания книги.
+    """
+    def __init__(self, title, author, year):
+        """
+        Инициализирует объект Book с указанными параметрами.
+
+        Args:
+            title (str): Название книги.
+            author (str): Автор книги.
+            year (str): Год издания книги.
+        """
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def description(self):
+        """
+        Возвращает описание книги в виде строки.
+
+        Returns:
+            str: Строка с названием, автором и годом издания книги.
+        """
+        return f"Название: {self.title}; Автор: {self.author}; Год издания: {self.year}."
+
+
+Book1 = Book("Атака на титанов ч.1", "Хадзимэ Исаяма", "2009")
+print(Book1.description())
